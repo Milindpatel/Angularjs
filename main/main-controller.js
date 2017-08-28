a89a9c75543223857dd8cebf5dfeb5570c7ea390
Milindpatel <milindpatel3959@gmail.com>
2017-08-28 00:32:09 -0400

    All the basic functionality of angular js are added

---
+++ main/main-controller.js
@@ -0,0 +1,11 @@
+angular.module('myApp').controller('MainController', MainController);
+
+function MainController(FilmFactory){
+    var vm = this;
+
+    FilmFactory.getAllFilms().then(function(response){
+        vm.films = response;
+    })
+    
+    vm.name = 'Milind';
+}
